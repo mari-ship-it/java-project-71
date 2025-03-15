7e1a4a75efb687746de6c810467a5b328484a08c
Marina Sirenko <msirenko96@gmail.com>
2025-03-15 19:46:18 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,16 +1,28 @@
 package hexlet.code;
 
 import picocli.CommandLine;
-//import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true, /*helpCommand = true,*/
+import java.io.File;
+
+import static java.awt.Color.red;
+import static picocli.CommandLine.Help.Ansi.Style.fg;
+
+@Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true, sortOptions = false,
         description = "Compares two configuration files and shows a difference.")
 final class App implements Runnable {
 
-    //@Parameters(index = "0")
-    //private String getHelp;
+    @Parameters(index = "0",  paramLabel = "filepath1", description = "path to first file")
+    private File filePath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private File filePath2;
+
+    @Option(names = {"-f", "--format"},paramLabel = "format",
+            description = "output format @|bold [|@ default: stylish]", defaultValue = "stylish")
+    String format;
 
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this @|blue help|@ message and exit.")
     boolean usageHelpRequested;
