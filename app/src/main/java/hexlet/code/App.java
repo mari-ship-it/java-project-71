4b33a8ba1b6a28c773068d748b59ce855d6f2d5d
Marina Sirenko <msirenko96@gmail.com>
2025-03-16 14:26:38 +0300

    implement reading and parsing

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,40 +4,56 @@ import picocli.CommandLine;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import java.util.concurrent.Callable;
 
-import java.io.File;
-
-import static java.awt.Color.red;
-import static picocli.CommandLine.Help.Ansi.Style.fg;
-
-@Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true, sortOptions = false,
+@Command(name = "gendiff",
+        version = "gendiff 1.0",
+        mixinStandardHelpOptions = true,
+        sortOptions = false,
         description = "Compares two configuration files and shows a difference.")
-final class App implements Runnable {
 
-    @Parameters(index = "0",  paramLabel = "filepath1", description = "path to first file")
-    private File filePath1;
+final class App implements Callable<Integer> {
 
-    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
-    private File filePath2;
+    @Parameters(index = "0",
+            paramLabel = "filepath1",
+            description = "path to first file")
+    String filePath1;
 
-    @Option(names = {"-f", "--format"},paramLabel = "format",
-            description = "output format @|bold [|@ default: stylish]", defaultValue = "stylish")
-    String format;
+    @Parameters(index = "1",
+            paramLabel = "filepath2",
+            description = "path to second file")
+    String filePath2;
 
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this @|blue help|@ message and exit.")
-    boolean usageHelpRequested;
+    @Option(names = {"-f", "--format"},
+            paramLabel = "format",
+            defaultValue = "stylish",
+            description = "output format @|bold [|@ default: stylish]")
+    private String format;
 
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-    boolean versionInfoRequested;
+    @Option(names = {"-h", "--help"},
+            usageHelp = true,
+            description = "Show this @|blue help|@ message and exit.")
+    private boolean usageHelpRequested;
+
+    @Option(names = {"-V", "--version"},
+            versionHelp = true,
+            description = "Print version information and exit.")
+    private boolean versionInfoRequested;
+
+    @Override
+    public Integer call() {
+
+        try {
+            Differ.generate(filePath1, filePath2);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return 0;
+    }
 
     public static void main(String[] args) {
 
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
-        //System.out.println("Hello World!");
-    }
-
-    @Override
-    public void run() {
     }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,8 +1,29 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Differ {
 
-    public static void generate() {
+    public static void generate(String filePath1, String filePath2) throws Exception {
 
+        Path path1 = Paths.get("src", "test", "resources", filePath1).toAbsolutePath().normalize();
+        Path path2 = Paths.get("src", "test", "resources", filePath2).toAbsolutePath().normalize();
+
+        String json1 = Files.readString(path1).trim();
+        String json2 = Files.readString(path2).trim();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map1 = objectMapper.readValue(json1, new TypeReference<Map<String, Object>>(){});
+        Map<String, Object> map2 = objectMapper.readValue(json2, new TypeReference<Map<String, Object>>(){});
+
+        System.out.println(map1);
+        System.out.println(map2);
     }
 }
