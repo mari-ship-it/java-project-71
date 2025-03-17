f7bde15b6d4edf0c0be8641c383ce6d5623641c6
Marina Sirenko <msirenko96@gmail.com>
2025-03-17 21:01:56 +0300

    add Utils.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,10 +1,8 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 import java.util.TreeSet;
 
@@ -12,13 +10,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Differ {
 
-    public static String generate(String filePath1, String filePath2) throws Exception {
+    public static String generate(String filePath1, String filePath2) throws JsonProcessingException {
 
-        Path path1 = Paths.get("src", "test", "resources", filePath1).toAbsolutePath().normalize();
-        Path path2 = Paths.get("src", "test", "resources", filePath2).toAbsolutePath().normalize();
-
-        String json1 = Files.readString(path1).trim();
-        String json2 = Files.readString(path2).trim();
+        String json1 = Utils.readFile(filePath1);
+        String json2 = Utils.readFile(filePath2);
 
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, Object> map1 = objectMapper.readValue(json1, new TypeReference<Map<String, Object>>() { });
---
+++ app/src/main/java/hexlet/code/Utils.java
@@ -0,0 +1,26 @@
+package hexlet.code;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+class Utils {
+
+    private static Path getPath(String fileName) {
+
+        return Paths.get("src", "test", "resources", "fixtures", fileName).toAbsolutePath().normalize();
+    }
+
+    protected static String readFile(String fileName) {
+
+        Path path = getPath(fileName);
+        try {
+
+            return Files.readString(path).trim();
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+        }
+    }
+}
