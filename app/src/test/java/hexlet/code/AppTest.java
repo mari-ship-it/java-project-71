cb8adf33253a42982993b7e2651466c5666d52f1
Marina Sirenko <msirenko96@gmail.com>
2025-03-19 16:29:03 +0300

    update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,19 +1,37 @@
 
 package hexlet.code;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static hexlet.code.Differ.generate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
 
-    @Test
-    public void testDiffGen() throws JsonProcessingException {
+    private static String expected;
 
-        String expected = Utils.readFile("result.json");
+    @BeforeAll
+    public static void beforeAll() {
+        expected = Parser.readFile("result.json");
+    }
+
+    public static AppTest createAppTest() {
+        return new AppTest();
+    }
+
+    @Test
+    public void testDiffGen() throws Exception {
+
         System.out.println(expected);
-        String actual = Differ.generate("file1.json", "file2.json");
+        String actual = generate("file1.json", "file2.json");
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testYml() throws Exception {
+
+        String actual2 = Differ.generate("filepath1.yml", "filepath2.yml");
+        assertEquals(expected, actual2);
     }
 }
