7f02f1c72b2ce4313d9eeac039966fe3819393f2
Marina Sirenko <msirenko96@gmail.com>
2025-03-19 16:30:08 +0300

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,23 +1,32 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-
 import java.util.Map;
 import java.util.TreeSet;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
 
 public class Differ {
 
-    public static String generate(String filePath1, String filePath2) throws JsonProcessingException {
+    private static Map<String, Object> getMap(String fileName) throws Exception {
 
-        String json1 = Utils.readFile(filePath1);
-        String json2 = Utils.readFile(filePath2);
+        String formatFile = fileName.substring(fileName.lastIndexOf('.') + 1);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> map1 = objectMapper.readValue(json1, new TypeReference<Map<String, Object>>() { });
-        Map<String, Object> map2 = objectMapper.readValue(json2, new TypeReference<Map<String, Object>>() { });
+        ObjectMapper mapper = switch (formatFile) {
+            case "yaml", "yml" -> new YAMLMapper();
+            case "json" -> new ObjectMapper();
+            default -> throw new IllegalStateException("Unexpected value: " + formatFile);
+        };
+
+        return mapper.readValue(Parser.readFile(fileName),
+                new TypeReference<Map<String, Object>>() { });
+    }
+
+    public static String generate(String filePath1, String filePath2) throws Exception {
+
+        Map<String, Object> map1 = getMap(filePath1);
+        Map<String, Object> map2 = getMap(filePath2);
 
         TreeSet<String> keys = new TreeSet<>(map1.keySet());
         keys.addAll(map2.keySet());
@@ -26,7 +35,7 @@ public class Differ {
         StringBuilder genDiff = new StringBuilder();
         genDiff.append("{\n");
 
-        sortedKeys.forEach((key) -> {
+        for (String key : sortedKeys) {
 
             if (!map1.containsKey(key)) {
 
@@ -43,7 +52,7 @@ public class Differ {
                 genDiff.append("  - ").append(key).append(": ").append(map1.get(key)).append("\n");
                 genDiff.append("  + ").append(key).append(": ").append(map2.get(key)).append("\n");
             }
-        });
+        }
 
         genDiff.append("}");
         return genDiff.toString();
