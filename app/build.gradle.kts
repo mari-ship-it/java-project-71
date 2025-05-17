2278b149da5562e620558444dba83eec2c83aae7
Marina Sirenko <msirenko96@gmail.com>
2025-05-17 17:56:04 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,42 @@
 plugins {
-    id("java")
+    application
+    jacoco
     checkstyle
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
+    implementation("info.picocli:picocli:4.7.6")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
+
+tasks.test{
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+sonar {
+    properties {
+        property("sonar.projectKey", "mari-ship-it_java-project-71")
+        property("sonar.organization", "mari-ship-it")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
