27fa07121b376348e20b2b04b5543ea6cd227b08
Marina Sirenko <msirenko96@gmail.com>
2025-05-22 11:35:00 +0300

    add Parser.java

---
+++ app/src/main/java/hexlet/code/Parser.java
@@ -0,0 +1,20 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import java.util.Map;
+
+public class Parser {
+
+    public static Map<String, Object> parse(String content, String format) throws Exception {
+
+        ObjectMapper mapper = switch (format) {
+            case "yaml", "yml" -> new YAMLMapper();
+            case "json" -> new ObjectMapper();
+            default -> throw new IllegalStateException("Unexpected value: " + format);
+        };
+
+        return mapper.readValue(content, new TypeReference<>() {});
+    }
+}
