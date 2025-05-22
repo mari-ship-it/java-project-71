3788399ea598d86381e7f376d7a8a4feb0f7c75f
Marina Sirenko <msirenko96@gmail.com>
2025-05-22 11:30:58 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     checkstyle
     id("com.github.ben-manes.versions") version "0.51.0"
     id("org.sonarqube") version "6.2.0.5505"
+    id("io.freefair.lombok") version "8.4"
 }
 
 group = "hexlet.code"
@@ -18,13 +19,13 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.3")
     implementation("info.picocli:picocli:4.7.6")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
-
 
 tasks.test{
     useJUnitPlatform()
