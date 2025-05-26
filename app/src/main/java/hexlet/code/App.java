17f25a637abb24c68b36d529d7d21c6ef501de14
Marina Sirenko <msirenko96@gmail.com>
2025-05-26 23:34:51 +0300

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,16 +4,13 @@ import picocli.CommandLine;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-import java.io.IOException;
 import java.util.concurrent.Callable;
-import java.util.logging.Logger;
+
 
 @Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true, sortOptions = false,
         description = "Compares two configuration files and shows a difference.")
 
 final class App implements Callable<Integer> {
-
-    Logger logger = Logger.getLogger(getClass().getName());
 
     @Parameters(index = "0", paramLabel = "filePath1", description = "path to first file")
     private String filePath1;
@@ -32,10 +29,9 @@ final class App implements Callable<Integer> {
     private boolean versionInfoRequested;
 
     @Override
-    public Integer call() throws IOException {
-
+    public Integer call() {
         try {
-            logger.info(Differ.generate(filePath1, filePath2, format));
+            System.out.println(Differ.generate(filePath1, filePath2, format));
         } catch (Exception e) {
             throw new IllegalArgumentException("Ошибка при сравнении файлов.");
         }
