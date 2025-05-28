8f222733597f6f27248bf6c5cf0314be3eb1a662
Marina Sirenko <msirenko96@gmail.com>
2025-05-28 22:59:18 +0300

    update AppTest.java

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,9 +1,13 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,59 +16,57 @@ class AppTest {
     private static String expectedStylish;
     private static String expectedPlain;
     private static String expectedJson;
+    private static final String PATH_TO_FILE = "src/test/resources/fixtures/";
+
+    private static String readFileTest(String fileName) throws IOException {
+        Path path = Paths.get(PATH_TO_FILE + fileName).toAbsolutePath().normalize();
+        return Files.readString(path).trim();
+    }
 
     @BeforeAll
     static void beforeAll() throws IOException {
-        expectedStylish = Differ.readFile("src/test/resources/fixtures/stylish.yaml");
-        expectedPlain = Differ.readFile("src/test/resources/fixtures/plain.yaml");
-        expectedJson = Differ.readFile("src/test/resources/fixtures/json.json");
+        expectedStylish = readFileTest("stylish.yaml");
+        expectedPlain = readFileTest("plain.yaml");
+        expectedJson = readFileTest("json.json");
     }
 
-    @Test
-    void testJsonWithStylishOutput() {
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    void testDefaultOutput(String inputFormat) {
 
-        String actual = Differ.generate("src/test/resources/fixtures/file1.json",
-                "src/test/resources/fixtures/file2.json");
+        String file1 = PATH_TO_FILE + "file1." + inputFormat;
+        String file2 = PATH_TO_FILE + "file2." + inputFormat;
+        String actual = Differ.generate(file1, file2);
         assertEquals(expectedStylish, actual);
     }
 
-    @Test
-    void testWithStylishOutput() {
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    void testPlainOutput(String inputFormat) {
 
-        String actual2 = Differ.generate("src/test/resources/fixtures/file1.yaml",
-                "src/test/resources/fixtures/file2.yaml");
-        assertEquals(expectedStylish, actual2);
-    }
-
-    @Test
-    void testJsonWithPlainOutput() {
-
-        String actual = Differ.generate("src/test/resources/fixtures/file1.json",
-                "src/test/resources/fixtures/file2.json", "plain");
+        String file1 = PATH_TO_FILE + "file1." + inputFormat;
+        String file2 = PATH_TO_FILE + "file2." + inputFormat;
+        String actual = Differ.generate(file1, file2, "plain");
         assertEquals(expectedPlain, actual);
     }
 
-    @Test
-    void testYamlWithPlainOutput() {
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    void testJsonOutput(String inputFormat) {
 
-        String actual = Differ.generate("src/test/resources/fixtures/file1.yaml",
-                "src/test/resources/fixtures/file2.yaml", "plain");
-        assertEquals(expectedPlain, actual);
-    }
-
-    @Test
-    void testJsonWithJsonOutput() {
-
-        String actual = Differ.generate("src/test/resources/fixtures/file1.json",
-                "src/test/resources/fixtures/file2.json", "json");
+        String file1 = PATH_TO_FILE + "file1." + inputFormat;
+        String file2 = PATH_TO_FILE + "file2." + inputFormat;
+        String actual = Differ.generate(file1, file2, "json");
         assertEquals(expectedJson, actual);
     }
 
-    @Test
-    void testYamlWithJsonOutput() {
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yaml"})
+    void testStylishOutput(String inputFormat) {
 
-        String actual = Differ.generate("src/test/resources/fixtures/file1.yaml",
-                "src/test/resources/fixtures/file2.yaml", "json");
-        assertEquals(expectedJson, actual);
+        String file1 = PATH_TO_FILE + "file1." + inputFormat;
+        String file2 = PATH_TO_FILE + "file2." + inputFormat;
+        String actual = Differ.generate(file1, file2, "stylish");
+        assertEquals(expectedStylish, actual);
     }
 }
